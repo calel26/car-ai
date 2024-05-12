3e71f93fc751955228062b0f663f6dbd24ad1525
Lawson Cale <calel26@episcopalacademy.org>
2024-05-12 19:54:09 -0400

    added program

---
+++ cart.py
@@ -0,0 +1,33 @@
+import gymnasium as gym
+import multiprocessing
+
+from stable_baselines3 import PPO
+from stable_baselines3.common.vec_env import SubprocVecEnv
+
+timesteps = 1_000_000
+
+# multiprocessing
+def make_env(rank, seed=0):
+    def _make():
+        env = gym.make("CarRacing-v2", render_mode="rgb_array")
+        env.reset(seed=seed + rank)
+        return env
+
+    return _make
+
+
+def start():
+    nproc = multiprocessing.cpu_count()
+
+    env = SubprocVecEnv([make_env(i) for i in range(nproc)])
+
+    # cnn policy because it's vision!
+    model = PPO("CnnPolicy", env, verbose=1)
+    model.learn(total_timesteps=timesteps, progress_bar=True)
+    model.save("out/selfdrivingcar")
+    vec_env = model.get_env()
+    obs = vec_env.reset()
+
+
+if __name__ == "__main__":
+    start()
